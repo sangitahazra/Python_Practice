69b0f6e09f29dc3b879037f25c838daf8e4ccc88
sangitahazra <67910565+sangitahazra@users.noreply.github.com>
2022-11-17 18:46:32 +0530

    Add files via upload

---
+++ sangita.py
@@ -0,0 +1,13 @@
+# Write your code here :-)
+# This program says hello and asks for my name.
+print('Hello, world!')
+print('What is your name?')
+myname=input()
+print('Your name is ' +myname)
+print('Enter your age =')
+myage=input()
+print('your age is '+myage)
+print('your age will be '+str(int(myage)+1)+' in a year.')
+print(len(myname))
+
+
